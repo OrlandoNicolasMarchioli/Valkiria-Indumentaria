1f02d89d857d38c38399af07d343e2e42b1a7d0a
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2021-08-25 20:14:09 -0300

    igreso de datos

---
+++ constructor.js
@@ -0,0 +1,79 @@
+
+
+function Producto(nombre,precio,stock){
+    this.nombre = nombre;
+    this.precio = parceFloat(precio);
+    this.stock = stock; 
+    this.cantidad = function(){
+        alert(" El producto " + nombre + " tiene un precio de " + precio + " pesos  Y contamos con una cantidad de " + stock + " unidades ")
+    }
+    this.sumaIva = function() {
+        this.precio = this.precio * 1.21;
+    }
+    
+    
+}
+function agregarCarrito(producto){
+    monto = []
+    carritoFinal = []
+    if(producto == String){
+        for(producto in productos){    
+            if (Producto in productos){
+                cantidad = parceInt(prompt( "Ingrese la cantidad que desea comprar"))
+                if (cantidad < stock){
+                    total = cantidad * precio
+                    stock = stock - cantidad
+                    producto = prompt("Ingrese el nombre del siguiente producto a comprar (en caso de no querer mas ingrese 0)")
+                    monto.push(total)
+                    carritoFinal.push(Producto)
+
+                    if(producto == "0"){
+                        break
+                    }
+                }
+                else{
+                    alert("La cantidad ingresada es mayor al stock actual")
+                    cantidad = parceInt(prompt( "Ingrese la cantidad que desea comprar"))
+                }
+            }
+        
+            else{
+                alert("El producto no se encuenta en stock")
+                producto = toLowerCase(prompt("Ingrese otro producto que desee comprar"))
+            }
+        }
+        
+    }
+    else{
+        alert("El nombre ingresado no es valido");
+        producto = toLowerCase(prompt("Ingrese otro producto que desee comprar"))
+    }
+    alert("El monto total con impuestos incluidos es: " + monto * 1.21)
+    alert("Los productos comprados son: " + carritoFinal)
+}
+
+const productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10]
+
+
+
+/* Ingreso de productos */
+const producto1 = new Producto("mandarinas", 20 , 300);
+const producto2 = new Producto("peras", 40 , 350);
+const producto3 = new Producto("manzanas", 30 , 150);
+const producto4 = new Producto("uvas", 80 , 500);
+const producto5 = new Producto("zandia", 50 , 225);
+const producto6 = new Producto("pomelo", 45 , 375);
+const producto7 = new Producto("naranja", 25 , 390);
+const producto8 = new Producto("arroz", 20 , 100);
+const producto9 = new Producto("pan", 20 , 300);
+const producto10 = new Producto("fideos", 15 , 50);
+
+
+producto = toLowerCase(prompt("Ingrese otro producto que desee comprar"))
+agregarCarrito(producto)
+
+
+
+
+
+
