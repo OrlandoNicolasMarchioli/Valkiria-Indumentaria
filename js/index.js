aaabf0e34f7fefbb0646563af102de48f8bf2750
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2021-09-08 20:08:18 -0300

    desarrollo carrito

--- js/index.js
+++ js/index.js
@@ -1,18 +1,4 @@
 
-function enviarAlCarrito(){
-    /* Funcion que recibe el click del usuario y envia el producto al carrito */
-    
-    let button = document.getElementsByClassName("boton__compras")
-    button.addEventListener("click", enviarAlCarrito)
-    let li = document.createElement("li")
-    document.CarritoDeCompras__ul.appendChild("li")
-    for(producto of productos){
-        document.CarritoDeCompras__ul.appendChild("producto")
-    }    
-    
-}
-
-
 
 function Productos(nombre,precio,stock){
     this.nombre = nombre;
@@ -22,9 +8,31 @@ function Productos(nombre,precio,stock){
 
 
 const producto1 = new Productos("Sweater Wolf","$3500",10)
-const producto1 = new Productos("Jean1","$3500",10)
-const producto1 = new Productos("Jean2 Wolf","$3500",10)
-const producto1 = new Productos("Sweater2 Wolf","$3500",10)
-const producto1 = new Productos("Sweater3 Wolf","$3500",10)
-const producto1 = new Productos("Sweater4 Wolf","$3500",10)
+const producto2 = new Productos("Jean1","$3500",10)
+const producto3 = new Productos("Jean2 Wolf","$3500",10)
+const producto4 = new Productos("Sweater2 Wolf","$3500",10)
+const producto5 = new Productos("Sweater3 Wolf","$3500",10)
+const producto6 = new Productos("Sweater4 Wolf","$3500",10)
+
+let listaProductos = [producto1, producto2,producto3,producto4,producto5,producto6]
+
+Productos.forEach(producto => {
+    listaProductos +=
+    <div class="card">
+        <img src="../img/productos/index/1.jpeg" class="card__img__top">
+        </img>
+        <div class= "card__body">
+            <h5 class ="card__title">
+                ${producto.nombre}
+            </h5>
+            <p class="card-text">Precio: ${producto.precio}</p>
+            <p class="card-text">Precio: ${producto.stock}</p>
+            <button class = "btn btn-primary"
+                oneclick = "agregarAlCarrito('$(producto.nombre)')">Agregar al carrito
+            </button>
+        </div>
+    </div>    
+})
+const contenedor = document.getElementsByClassName("CarritoDeCompras__ul")
+contenedor.innerHTML = listaProductos 
 
