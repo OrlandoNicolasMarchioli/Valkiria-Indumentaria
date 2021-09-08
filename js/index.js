478cd899c96a79ef7751ef1d7de113300ec921bd
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2021-09-07 23:40:47 -0300

    cosas

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 
+let button = document.getElementsByClassName("boton__compras")
+button.addEventListener("click", enviarAlCarrito)
+function enviarAlCarrito(){
+    document.CarritoDeCompras__ul.appendChild()
+}
 
