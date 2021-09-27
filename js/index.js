f9750189b00c888b4e8e08a25463e65531a030eb
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2021-09-27 12:58:08 -0300

    trabajo boton carrito

--- js/index.js
+++ js/index.js
@@ -7,14 +7,14 @@ function Productos(img,nombre,precio){
 
 }
 
-const producto1 = new Productos("../img/productos/index/1.jpeg","Sweater Wolf","$2000")
-const producto2 = new Productos("../img/productos/index/2.jpeg","Jean 80's Syle","$3500")
-const producto3 = new Productos("../img/productos/index/3.jpeg","Jean light blue","$3200")
-const producto4 = new Productos("../img/productos/jeanRoto2.jpg","Jean Dark blue","$3500")
-const producto5 = new Productos("../img/productos/jeanRotoNegro.jpg","Sweater3 Wolf","$3500")
-const producto6 = new Productos("../img/productos/sweater2.jpg","Sweater4 Wolf","$3500")
-const producto7 = new Productos("../img/productos/combo1.jpg","Sweater4 Wolf","$3500")
-const producto8 = new Productos("../img/productos/combo2.jpg","Sweater4 Wolf","$3500")
+const producto1 = new Productos("../img/productos/index/1.jpeg","Sweater Wolf","2000")
+const producto2 = new Productos("../img/productos/index/2.jpeg","Jean 80's Syle","3500")
+const producto3 = new Productos("../img/productos/index/3.jpeg","Jean light blue","3200")
+const producto4 = new Productos("../img/productos/jeanRoto2.jpg","Jean Dark blue","3500")
+const producto5 = new Productos("../img/productos/jeanRotoNegro.jpg","Sweater3 Wolf","3500")
+const producto6 = new Productos("../img/productos/sweater2.jpg","Sweater4 Wolf","3500")
+const producto7 = new Productos("../img/productos/combo1.jpg","Sweater4 Wolf","3500")
+const producto8 = new Productos("../img/productos/combo2.jpg","Sweater4 Wolf","3500")
 
 
 /* Lista de productos */
@@ -34,7 +34,7 @@ listaProductos.forEach(producto => {
         <div class= "col__bolsa">
             <h5><b>${producto.precio}</b></h5>            
             <button class="botonCarrito boton__compras"
-                onclick = "agregarCarrito(${producto.nombre})">Comprar
+                onclick = "agregarCarrito('${producto.nombre}')">Comprar
             </button>
         </div>
     </div>     
@@ -51,21 +51,58 @@ let boton = document.getElementsByClassName("botonCarrito");
 let total = 0
 function agregarCarrito(seleccion){
     let encontrado = listaProductos.find(producto => producto.nombre == seleccion);
-    const card = `<h5 class="productoComprado">${encontrado.nombre}</h5>
-                <h6 class="productoprecio">${encontrado.precio}</h6>
-                <p>Precio total: ${total+= encontrado.precio}</p>`
+    const card = `
+    <div class= "carritoContainer">
+        <h5 class="productoComprado">${encontrado.nombre}</h5>
+        <h6 class="productoprecio">${encontrado.precio}</h6>
+        <p>Precio total: ${total+= encontrado.precio}</p>
+    </div>`
     console.log(encontrado)
 
-    let carro = document.getElementById('carritoDeCompras')
+    let carro = document.getElementById('botonCarrito')
     carro.innerHTML += card
 }
+
+
+
+
+
+/* Nav bar */
 
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 
 toggleButton.addEventListener("click", ()=> {
     navbarLinks.classList.toggle("active")
+}) 
+
+/* Carrousel */
+
+(function(){
+    const sliders = [...document.querySelector(".slider__body")];
+    const arrowAfter = document.querySelector("#after")
+    const arrowBefore = document.querySelector("#before")
+    let value = 0;
+    console.log(value)
+    arrowAfter.addEventListener("click", () => changePosition(1))
+    arrowBefore.addEventListener("click", () => changePosition(-1))
+
+    function changePosition(change){
+        const currentElement = Number(document.querySelector(".slider__body--show").dataset.id);
+
+        value = currentElement;
+        value += change;
+        
+        if(value === 0 || value == sliders.length+1){
+            value = value === 0 ? sliders.length :1;
+        }
+        sliders[currentElement -1].classList.toggle("slider__body--show");
+        sliders[value-1].classList.toggle("slider__body--show");
+        
+    }
 })
+
+/* const buttonPrev = document.get */
 
 
 
